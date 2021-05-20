b3ea278b336ce6ce23ff964dae5ee267936302ba
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-05-20 21:24:05 +0200

    RED: greet capitalizes input

--- test_greeter.py
+++ test_greeter.py
@@ -4,13 +4,16 @@ from greeter import Greeter
 
 
 class TestGreeter(unittest.TestCase):
+    expected = 'Hello Albert'
+
     def test_greet_should_say_hello(self):
         actual = Greeter.greet('Albert')
-        expected = 'Hello Albert'
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, self.expected)
 
     def test_greet_should_trim_input(self):
         actual = Greeter.greet('   Albert   ')
-        expected = 'Hello Albert'
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, self.expected)
 
+    def test_greet_should_capitalize_input(self):
+        actual = Greeter.greet('albert')
+        self.assertEqual(actual, self.expected)
