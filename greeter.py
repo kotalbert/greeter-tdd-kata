68d33adf933b8ac227b9e42702c41e61387d261c
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-05-20 21:28:01 +0200

    BLUE: extract string operations to method

--- greeter.py
+++ greeter.py
@@ -1,4 +1,9 @@
 class Greeter:
     @staticmethod
     def greet(name: str) -> str:
-        return f'Hello {name.strip().capitalize()}'
+        name_trans = Greeter._transform_name(name)
+        return f'Hello {name_trans}'
+
+    @staticmethod
+    def _transform_name(name: str) -> str:
+        return name.strip().capitalize()
