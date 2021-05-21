f6ea9e65eeff77ebcfa88a3476bb02948a79719d
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-05-21 21:00:41 +0200

    BLUE: refactor mocks in unit test to use patcher in setup

--- test_greeter.py
+++ test_greeter.py
@@ -3,12 +3,21 @@ from unittest import mock
 from unittest.mock import Mock
 from greeter import Greeter
 import greeter
+from unittest.mock import patch
 
 greeter.time = Mock()
 
 
 class TestGreeter(unittest.TestCase):
     expected = 'Hello Albert'
+
+    def setUp(self) -> None:
+        patcher = patch('greeter.time.localtime')
+        self.mock_localtime = patcher.start()
+        self.addCleanup(patcher.stop)
+
+    def set_hour(self, hour: int):
+        self.mock_localtime.return_value.tm_hour = hour
 
     def test_greet_should_say_hello(self):
         actual = Greeter.greet('Albert')
@@ -22,10 +31,8 @@ class TestGreeter(unittest.TestCase):
         actual = Greeter.greet('albert')
         self.assertEqual(actual, self.expected)
 
-    @mock.patch('greeter.time.localtime')
-    def test_greet_should_say_good_morning(self, mock_lt):
-        instance = mock_lt.return_value
-        instance.tm_hour = 10
+    def test_greet_should_say_good_morning(self):
+        self.set_hour(10)
 
         actual = Greeter.greet('Albert')
         expected = 'Good morning Albert'
