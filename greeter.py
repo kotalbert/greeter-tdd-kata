227ffbcccd17b5531cff435f4103b1d6ed70c7dc
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-05-22 15:47:56 +0200

    BLUE: refactor:
    * Extract getting greetings to method
    * Add test docstrings
    * Extract getting test greeting to unit test method
    * Inline test greetings

--- greeter.py
+++ greeter.py
@@ -5,16 +5,20 @@ class Greeter:
     @staticmethod
     def greet(name: str) -> str:
         name = Greeter._transform_name(name)
-        greeting = 'Hello'
+        greeting = Greeter._get_greeting()
 
+        return f'{greeting} {name}'
+
+    @staticmethod
+    def _get_greeting() -> str:
+        greeting = 'Hello'
         if Greeter._current_hour_between(6, 12):
             greeting = 'Good morning'
         elif Greeter._current_hour_between(18, 22):
             greeting = 'Good evening'
         elif Greeter._current_hour_between(22, 6):
             greeting = 'Good night'
-
-        return f'{greeting} {name}'
+        return greeting
 
     @staticmethod
     def _transform_name(name: str) -> str:
@@ -26,10 +30,8 @@ class Greeter:
 
     @staticmethod
     def _current_hour_between(range_start: int, range_end: int) -> bool:
-        hours_number = abs(range_start - range_end) + 1
-        clock_slice = [-1]*hours_number
-        for i in range(hours_number):
-            clock_slice[i] = (range_start + i) % 24
-
         ch = Greeter._get_current_hour()
-        return ch in clock_slice
+        for i in range(abs(range_start - range_end) + 1):
+            if ((range_start + i) % 24) == ch:
+                return True
+        return False
--- test_greeter.py
+++ test_greeter.py
@@ -1,11 +1,7 @@
 import unittest
-from unittest.mock import Mock
 from unittest.mock import patch
 
-import greeter
 from greeter import Greeter
-
-greeter.time = Mock()
 
 
 class TestGreeter(unittest.TestCase):
@@ -20,34 +16,60 @@ class TestGreeter(unittest.TestCase):
         self.current_hour = patcher.start()
         self.addCleanup(patcher.stop)
 
+    def greet(self):
+        """Helper to make greeting easier."""
+
+        return Greeter.greet(self.name)
+
     def set_hour(self, hour: int):
+        """Helper to set mocked hour."""
+
         self.current_hour.return_value = hour
 
     def test_greet_should_say_hello(self):
-        # todo: replace with method
-        actual = Greeter.greet(self.name)
+        """
+        Greeter.greet should say 'Hello'
+        """
+
+        actual = self.greet()
         self.assertEqual(actual, self.hello)
 
     def test_greet_should_trim_input(self):
+        """
+        Greeter.greet should trim input.
+        """
+
         actual = Greeter.greet(f'   {self.name}   ')
         self.assertEqual(actual, self.hello)
 
     def test_greet_should_capitalize_input(self):
+        """
+        Greeter.greet should capitalize name.
+        """
+
         actual = Greeter.greet(self.name.lower())
         self.assertEqual(actual, self.hello)
 
     def test_greet_should_say_good_morning(self):
-        self.set_hour(10)
+        """
+        Greeter.greet should say 'Good morning' in the morning (6:00-12:00)
+        """
 
-        actual = Greeter.greet(self.name)
-        self.assertEqual(actual, self.gm)
+        self.set_hour(10)
+        self.assertEqual(self.greet(), self.gm)
 
     def test_greet_should_say_good_evening_in_evening(self):
+        """
+        Greeter.greet should say 'Good evening' in the evening (18:00-22:00)
+        """
+
         self.set_hour(20)
-        actual = Greeter.greet(self.name)
-        self.assertEqual(actual, self.ge)
+        self.assertEqual(self.greet(), self.ge)
 
     def test_greet_should_say_good_night_at_night(self):
+        """
+        Greeter.greet should say 'Good night' at night (22:00-6:00)
+        """
+
         self.set_hour(2)
-        actual = Greeter.greet(self.name)
-        self.assertEqual(actual, self.gn)
+        self.assertEqual(self.greet(), self.gn)
